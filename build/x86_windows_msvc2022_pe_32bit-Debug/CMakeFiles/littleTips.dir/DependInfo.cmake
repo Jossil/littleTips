89db0cf840cb3e34444f8a8d573a187edba68ea5
O <525610254@qq.com>
2025-04-28 14:08:04 +0800

    Initial commit

---
+++ build/x86_windows_msvc2022_pe_32bit-Debug/CMakeFiles/littleTips.dir/DependInfo.cmake
@@ -0,0 +1,26 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "" "littleTips_autogen/timestamp" "custom" "littleTips_autogen/deps"
+  "D:/gitProject/littleTips/build/x86_windows_msvc2022_pe_32bit-Debug/littleTips_autogen/mocs_compilation.cpp" "CMakeFiles/littleTips.dir/littleTips_autogen/mocs_compilation.cpp.obj" "msvc" "CMakeFiles/littleTips.dir/littleTips_autogen/mocs_compilation.cpp.obj.d"
+  "D:/gitProject/littleTips/main.cpp" "CMakeFiles/littleTips.dir/main.cpp.obj" "msvc" "CMakeFiles/littleTips.dir/main.cpp.obj.d"
+  "D:/gitProject/littleTips/mainwindow.cpp" "CMakeFiles/littleTips.dir/mainwindow.cpp.obj" "msvc" "CMakeFiles/littleTips.dir/mainwindow.cpp.obj.d"
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_FORWARD_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ mainwindow.cpp
@@ -0,0 +1,72 @@
+#include "mainwindow.h"
+#include "./ui_mainwindow.h"
+#include <QPushButton>
+#include <QTextEdit>
+#include <QTableWidget>
+#include <QDateTime>
+#include <QDebug>
+
+MainWindow::MainWindow(QWidget *parent)
+    : QMainWindow(parent)
+    , ui(new Ui::MainWindow)
+{
+    ui->setupUi(this);
+    // 设置置顶
+    m_originalFlags = windowFlags();
+    static int height = ui->tableWidget->height();
+    // 创建一个切换置顶状态的按钮
+    setWindowFlags(windowFlags() | Qt::WindowStaysOnTopHint);
+    connect(ui->pushButton, &QPushButton::clicked, this, &MainWindow::addToDialog);
+    connect(ui->pushButton_2, &QPushButton::clicked, this, [=]{
+        auto pnp = this->height();
+        if(ui->tableWidget->isHidden()){
+            ui->tableWidget->show();
+            this->setFixedHeight(250);
+        }
+        else{
+            ui->tableWidget->hide();
+            this->setFixedHeight(130);
+        }
+    });
+    this->setFixedHeight(130);
+    ui->tableWidget->hide();
+    ui->tableWidget->resizeRowsToContents();   // 行高自适应
+}
+
+MainWindow::~MainWindow()
+{
+    delete ui;
+}
+
+void MainWindow::toggleAlwaysOnTop()
+{
+    if (windowFlags() & Qt::WindowStaysOnTopHint) {
+        // 当前是置顶状态，恢复原始状态
+        setWindowFlags(m_originalFlags);
+    } else {
+        // 当前不是置顶状态，设置为置顶
+        setWindowFlags(m_originalFlags | Qt::WindowStaysOnTopHint);
+    }
+    show(); // 必须调用show()才能使标志更改生效
+}
+
+void MainWindow::addToDialog(){
+    if(!ui->textEdit->toPlainText().isEmpty()){
+        auto p=ui->textEdit->toPlainText();
+        // 获取当前日期和时间（默认时区）
+        QDateTime currentDateTime = QDateTime::currentDateTime();
+        // 转换为字符串（格式化输出）
+        QString dateTimeString = currentDateTime.toString("yyyy-MM-dd hh:mm:ss");
+        QTableWidgetItem* timeitem = new QTableWidgetItem(dateTimeString);
+        QTableWidgetItem* contentitem = new QTableWidgetItem(p);
+        //ui->tableWidget->setRowCount(ui->tableWidget->rowCount()+1);
+        ui->tableWidget->insertRow(0);
+        ui->tableWidget->setItem(0, 0, timeitem);
+        ui->tableWidget->setItem(0, 1, contentitem);
+        timeitem->setFlags(timeitem->flags() & ~Qt::ItemIsEditable);
+        timeitem->setData(Qt::TextAlignmentRole, Qt::AlignLeft | Qt::AlignTop | Qt::TextWordWrap); // 启用换行
+        contentitem->setFlags(contentitem->flags() & ~Qt::ItemIsEditable);
+        contentitem->setData(Qt::TextAlignmentRole, Qt::AlignLeft | Qt::AlignTop | Qt::TextWordWrap); // 启用换行
+        ui->textEdit->clear();
+    }
+}
